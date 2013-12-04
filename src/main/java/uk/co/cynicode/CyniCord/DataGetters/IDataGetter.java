6f3208b614b6ea856d9497c6c86890304dd9f022
M477h3w1012 <m.477h3w@live.co.uk>
2013-12-04 23:06:58 +0000

    Javadoc the IDataGetter interface

--- src/main/java/uk/co/cynicode/CyniCord/DataGetters/IDataGetter.java
+++ src/main/java/uk/co/cynicode/CyniCord/DataGetters/IDataGetter.java
@@ -4,16 +4,43 @@ import java.util.Map;
 
 import uk.co.cynicode.CyniCord.CyniCord;
 
+/**
+ * An interface for all possible methods of getting the data about
+ * IRC channels
+ * @author Cynical
+ */
 public interface IDataGetter {
-
+	
+	/**
+	 * Start up the connection to whichever method you need
+	 * @param plugin : The instance of the plugin that might be used
+	 *  for something or another
+	 * @return whether it has connected or not
+	 */
 	public boolean startConnection( CyniCord plugin );
 	
+	/**
+	 * Kill the connection to whatever we're using
+	 * @return whether the method is dead or not
+	 */
 	public boolean endConnection();
 	
+	/**
+	 * Get a set of all the channels that are available
+	 * @return Map< IRC Channel Name, Minecraft Channel Name >
+	 */
 	public Map<String, String> getChannels();
 	
+	/**
+	 * Get all the IRC information about the channels
+	 * @return Map< IRC Channel Name, IRC Chanel Password >
+	 */
 	public Map<String, String> getIRCChannels();
 	
+	/**
+	 * Get all the loaded channels
+	 * @return the loaded channels
+	 */
 	public Map<String, String> loadChannels();
 	
 }
