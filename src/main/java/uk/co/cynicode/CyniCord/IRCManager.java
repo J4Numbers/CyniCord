b668a7e184eccbc1f489415bd01d4f951088db56
M477h3w1012 <m.477h3w@live.co.uk>
2013-12-04 23:02:30 +0000

    Comment and Javadoc the IRCManager class

--- src/main/java/uk/co/cynicode/CyniCord/IRCManager.java
+++ src/main/java/uk/co/cynicode/CyniCord/IRCManager.java
@@ -37,25 +37,41 @@ public class IRCManager {
 	 */
 	public IRCManager(CyniCord plugin/*, IDataGetter connection*/ ) throws Exception {
 		
-		port = plugin.getConfig().getInt("CyniCord.irc.port");
-		hostname = plugin.getConfig().getString("CyniCord.irc.hostname");
-		nickname = plugin.getConfig().getString("CyniCord.irc.nickname");
+		//Get the information about the IRC server we're going to
+		// connect to
+		this.port = plugin.getConfig().getInt("CyniCord.irc.port");
+		this.hostname = plugin.getConfig().getString("CyniCord.irc.hostname");
+		this.nickname = plugin.getConfig().getString("CyniCord.irc.nickname");
 		
-		CyniCord.printDebug( nickname + "@" + hostname + ":" + port);
+		//Print out the information as some nice debug bits
+		CyniCord.printDebug( this.nickname + "@" + this.hostname + ":" + this.port);
 		
+		//Make a new bot
 		this.bot = new PircBotX();
 		
 		//Map<String, String> channels = connection.loadChannels();
 		
+		//And then register the thing that will enable two-way chatter
 		this.bot.getListenerManager().addListener(new IRCChatListener());
-		this.bot.setName( nickname );
+		
+		//Set the name to whatever has been chosen
+		this.bot.setName( this.nickname );
+		
+		//Then set the login to my own since I like it :P
 		this.bot.setLogin("CyniBot");
 		
-		CyniCord.printDebug( "Trying to connect " + nickname + " to " + hostname + ":" + port );
+		//More debug dear lizer, dear lizer
+		CyniCord.printDebug( "Trying to connect " + this.nickname + " to " 
+			+ this.hostname + ":" + this.port );
 		
+		//Now actually do what the debug is saying we're doing
 		try {
-			this.bot.connect( hostname, port );
-			CyniCord.printInfo( "Connected " + nickname + " to IRC server: " + hostname );
+			
+			//And connect!
+			this.bot.connect( this.hostname, this.port );
+			
+			//Huh... that was easy
+			CyniCord.printInfo( "Connected " + this.nickname + " to IRC server: " + this.hostname );
 			
 			/*if ( channels != null ) {
 				Set<String> names = channels.keySet();
@@ -75,82 +91,213 @@ public class IRCManager {
 		}
 	}
 	
+	/**
+	 * Ask if we are joined to this given channel.
+	 * @param channel : The channel we are giving
+	 * @return true or false
+	 */
 	public boolean channelJoined( String channel ) {
-		return bot.channelExists( channel );
+		return getBot().channelExists( channel );
 	}
-
+	
+	/**
+	 * Join the channel.
+	 * @param name : This channel in fact
+	 * @param pass : With this password
+	 */
 	public void joinChannel( String name, String pass ) {
-		bot.joinChannel( name, pass );
+		getBot().joinChannel( name, pass );
 	}
-
+	
+	/**
+	 * Given a map of channels, join to all of them
+	 * @param channels : The channels we wish to join to
+	 * @param ircChans : The channels and their passwords
+	 */
 	public void addChannels( Map<String, String> channels, Map<String, String> ircChans ) {
 		
-		Set<String> keys = channels.keySet();
-		Iterator<String> iterKeys = keys.iterator();
-		
-		while ( iterKeys.hasNext() ) {
+		//Go through the channels
+		for ( Map.Entry<String, String> thisSet : channels.entrySet() ) {
 			
-			String thisIrcChan = iterKeys.next();
-			String thisGameChan = channels.get( thisIrcChan );
+			//And retrieve all the pertinent data
+			String thisIrcChan = thisSet.getKey();
+			String thisGameChan = thisSet.getValue();
 			String thisIrcPass = ircChans.get( thisIrcChan );
 			
-			if ( !this.ircChannelNames.containsValue(thisGameChan) ) {
-				this.ircChannelNames.put( thisIrcChan, thisGameChan);
-				this.gameChannelNames.put( thisGameChan, thisGameChan );
+			//Then ask if we already have the channels in the 
+			// maps of the class. If we do, move on. Otherwise
+			// we add them into the current lists.
+			if ( !getIrcChannelNames().containsValue(thisGameChan) ) {
+				getIrcChannelNames().put( thisIrcChan, thisGameChan);
+				getIrcChannelNames().put( thisGameChan, thisGameChan );
 			}
 			
+			//Now actually join the channels
 			try {
+				
+				//like so
 				CyniCord.printDebug( "Attempting to connect to " + thisIrcChan + " with password: " + thisIrcPass );
-				this.bot.joinChannel( thisIrcChan, thisIrcPass );
+				getBot().joinChannel( thisIrcChan, thisIrcPass );
+				
 			} catch ( Exception e ) {
+				
+				//Or not...
 				CyniCord.printSevere( "Failed to connect to the "+ thisIrcChan +" channel" );
 				e.printStackTrace();
+				
 			}
 			
 		}
+		
 	}
-
+	
+	/**
+	 * Send a message using the bot
+	 * @param channel : Given the channel
+	 * @param player : The player
+	 * @param message : And the message itself
+	 */
 	public void sendMessage( String channel, String player, String message ) {
+		
+		//Try just in case of error
 		try {
-			bot.sendMessage( channel, player + " : " + message );
+			
+			//Send the message
+			getBot().sendMessage( channel, player + " : " + message );
+			//And we're done
+			
 		} catch ( Exception e ) {
+			
+			//Or not...
 			CyniCord.printDebug( "Oops! Something went wrong!" );
 			e.printStackTrace();
+			
 		}
+		
 	}
 	
 	/**
 	 * Restart the IRC component of the plugin via various means
 	 */
 	public void restart() {
+		
+		//Tell the console what is happening
 		CyniCord.printWarning("Restarting the IRC bot...");
 		CyniCord.printInfo("Stopping the IRC bot...");
-		bot.shutdown();
+		
+		//And kill the bot
+		getBot().shutdown();
+		
+		//Then start it all up again
 		CyniCord.printInfo("Starting up the IRC bot again...");
 		try {
-			bot.reconnect();
+			
+			//Well... attempt to do so anyway
+			getBot().reconnect();
 			//loadChannels(DataManager.returnAllChannels());
+			
+			//And it's done
 			CyniCord.printInfo("Reconnected successfully");
+			
 		} catch (IOException e) {
+			
+			//These... are issues
 			CyniCord.printSevere("We could not connect...");
 			e.printStackTrace();
+			
 		} catch (NickAlreadyInUseException e) {
+			
+			//As is this
 			CyniCord.printSevere("Our nickname was already in use...");
 			e.printStackTrace();
+			
 		} catch (IrcException e) {
+			
+			//And that
 			CyniCord.printSevere("IRC has failed... call in the drones...");
 			e.printStackTrace();
+			
 		}
+		
 	}
-
+	
 	/**
 	 * Kill the IRC component of the plugin ungracefully ... In other words,
 	 * knock it over the head with a bat.
 	 */
 	public void stop() {
+		
+		//As it is said... stop the server
 		CyniCord.printInfo("Shutting down IRC...");
-		this.bot.shutdown(true);
+		
+		//With no chance of reconnection
+		getBot().shutdown(true);
+		
 		CyniCord.printInfo("IRC has been killed.");
+		
+	}
+	
+	/**
+	 * @return the instance of the bot
+	 */
+	public PircBotX getBot() {
+		return this.bot;
+	}
+	
+	/**
+	 * @return The map of IRC channels
+	 */
+	public Map<String,String> getIrcChannelNames() {
+		return this.ircChannelNames;
+	}
+	
+	/**
+	 * @return the hostname
+	 */
+	public String getHostname() {
+		return hostname;
+	}
+	
+	/**
+	 * @param hostname the hostname to set
+	 */
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+	
+	/**
+	 * @return the nickname
+	 */
+	public String getNickname() {
+		return nickname;
+	}
+	
+	/**
+	 * @param nickname the nickname to set
+	 */
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	/**
+	 * @return the port
+	 */
+	public int getPort() {
+		return port;
+	}
+	
+	/**
+	 * @param port the port to set
+	 */
+	public void setPort(int port) {
+		this.port = port;
+	}
+	
+	/**
+	 * @return the game channel names
+	 */
+	public Map<String, String> getGameChannelNames() {
+		return gameChannelNames;
 	}
 	
 }
