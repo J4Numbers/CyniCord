1ff6c1de5bc9418e43559c5562d1f7906f4b3f99
M477h3w1012 <m.477h3w@live.co.uk>
2013-12-16 16:29:42 +0000

    Added in licensing

--- src/main/java/uk/co/cynicode/CyniCord/DataGetters/MySQLDataGetter.java
+++ src/main/java/uk/co/cynicode/CyniCord/DataGetters/MySQLDataGetter.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2013 CyniCode (numbers@cynicode.co.uk).
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.co.cynicode.CyniCord.DataGetters;
 
 import java.sql.Connection;
@@ -15,7 +31,8 @@ import uk.co.cynicode.CyniCord.CyniCord;
 /**
  * A class to deal with all the MySQL data options that
  * are available for our nice viewers at home
- * @author Cynical
+ * 
+ * @author CyniCode
  */
 public class MySQLDataGetter implements IDataGetter {
 	
--- src/main/java/uk/co/cynicode/CyniCord/Listeners/PluginMessageListener.java
+++ src/main/java/uk/co/cynicode/CyniCord/Listeners/PluginMessageListener.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2013 CyniCode (numbers@cynicode.co.uk).
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.co.cynicode.CyniCord.Listeners;
 
 import java.io.ByteArrayInputStream;
@@ -13,7 +29,8 @@ import net.md_5.bungee.event.EventHandler;
 /**
  * A class responsible for listening out for plugin messages
  * and acting upon them as it sees fit.
- * @author Cynical
+ * 
+ * @author CyniCode
  */
 public class PluginMessageListener implements Listener {
 	
